e6a8f7b8e23dc11c35198b43ebbfc932459d9b5d
Jean Carlo <jeansanto2002@gmail.com>
2023-09-16 10:13:12 -0300

    commit inicial

---
+++ src/main/java/dev/jean/ApiRestNaNuvemComSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.jean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestNaNuvemComSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestNaNuvemComSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jean/ApiRestNaNuvemComSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestNaNuvemComSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
