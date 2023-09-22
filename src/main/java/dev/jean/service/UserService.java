a7ca9f61ad233a26aa5047c63997e04139d4649b
Jean Carlo <jeansanto2002@gmail.com>
2023-09-22 18:32:35 -0300

    Implementando os DTO e Exceções do Service

--- src/main/java/dev/jean/service/UserService.java
+++ src/main/java/dev/jean/service/UserService.java
@@ -2,7 +2,8 @@ package dev.jean.service;
 
 import dev.jean.domain.model.User;
 
-public interface UserService {
-    User findById(Long id);
-    User create(User userToCrete);
+public interface UserService extends CrudService<Long, User> {
+
 }
+
+
